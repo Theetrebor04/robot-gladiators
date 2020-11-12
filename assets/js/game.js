16046e71b2126c9fb11d7e3ae71c7c9f4568aed6
Robert Clawson <bobbycharizard@gmail.com>
2020-11-12 14:55:48 -0700

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,49 +8,28 @@ console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android ", "Rodo Trumble"];
 var enemyHealth = 50;
-var enemyAttack = 12;6
+var enemyAttack = 12;
 
 
 var fight = function(enemyName) {
     while(enemyHealth > 0 && playerHealth > 0) {
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'Skip' to choose. ");
 
-    // If player picks "skip" confirm and then stop the loop
-    if (promptFight === "skip" || promptFight === "SKIP") {
-        // Confirm player wants to skip 
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+        // If player picks "skip" confirm and then stop the loop
+        if (promptFight === "skip" || promptFight === "SKIP") {
+            // Confirm player wants to skip 
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
-        // if yes (true), leave fight
-        if (confirmSkip) {
-            window.alert(playerName + " has decided to skip this fight. Goodbye!");
-            //subtract money from playerMoney for skipping
-            playerMoney = playerMoney - 10;
-            console.log("playerMoney", playerMoney);
-            break;
-            }
+            // if yes (true), leave fight
+            if (confirmSkip) {
+                window.alert(playerName + " has decided to skip this fight. Goodbye!");
+                //subtract money from playerMoney for skipping
+                playerMoney = playerMoney - 10;
+                console.log("playerMoney", playerMoney);
+                break;
+            }       
         }
-    if (promptFight == "fight" || promptFight === "FIGHT") {
-
-    if (playerHealth > 0) {
-        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1) );
-
-        //pick new enemy to fight based on the index of the enemyNames array 
-        var pickedEnemyName = enemyNames[i];
-
-        //reset enemyHealth before starting new fight
-        enemyHealth = 50;
-
-        // use debugger to pause script from running and check what's going on at the moment in the code
-        // debugger;
-
-        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-        fight(pickedEnemyName);
-    }
-    else {
-        window.alert("You have lost your robot in battle! Game Over!");
-        break;
-    }
-
+    
         // Log a resulting message to the console so we know that it worked.
         enemyHealth = enemyHealth - playerAttack;
         console.log(
@@ -60,9 +39,10 @@ var fight = function(enemyName) {
         // Check enemy's health
         if (enemyHealth <=0) {
             window.alert(enemyName + " has died! ");
+            playerMoney = playerMoney +20;
             break;
         } else {
-         window.alert(enemyName + " still has " + enemyHealth + " health left. ")
+            window.alert(enemyName + " still has " + enemyHealth + " health left. ")
         }
 
         // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
@@ -79,13 +59,19 @@ var fight = function(enemyName) {
             break;
         } else {
             window.alert(playerName + " still has " + playerHealth + " health left.");
-        }
-    }    
-    }           
+        } 
+    }             
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1) );
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over");
+        break;
+    }
 }
